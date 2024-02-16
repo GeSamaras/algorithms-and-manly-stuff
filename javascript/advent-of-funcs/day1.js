790152cc0b69a3aafef48b48a5f959201a21fe4e
GeSamaras <georgiosamaras@hotmail.com>
2024-02-16 18:47:34 +0100

    day1 calorie counter fetch request for the puzzle input functional solution

--- javascript/advent-of-funcs/day1.js
+++ javascript/advent-of-funcs/day1.js
@@ -1,11 +1,42 @@
-const numberGroups = smapleInput.split("\n").map(Number);
+// fetch the puzzle input instead of hardcoding it
+const summonPuzzleInput = async () => {
+    // since this call is async, it will return a promise
+    const response = await fetch("https://adventofcode.com/2022/day/1/input");
+    const responseText = await response.text();
+    return responseText;
+};
 
-const sampleGroup = "1721\n979\n366\n299\n675\n1456";
+const puzzleInput = await summonPuzzleInput();
+
+const sumReducer = (sum, num) => sum + num;
 
 const getSumOfGroup = (group) => 
     group
         .split("\n")
         .map(Number)
-        .reduce((sum, num) => sum + num, 0);
+        .reduce(sumReducer, 0);
 
-console.log(getSumOfGroup(sampleGroup)); // 4678
+const numberGroups = puzzleInput.split("\n\n");
+
+// sum of each group
+const groupSums = numberGroups.map(getSumOfGroup);
+
+// largest sum
+const maxSum = Math.max(...groupSums);
+
+
+//Part 2, finding the top 3 sums
+
+// spread operator to make a copy of the array
+const sortedSums = [...groupSums].sort((num1, num2) => num2 - num1);
+
+
+// using slice to get the top 3 sums
+// first index is inclusive, second index is exclusive (0, 2) doesn't work
+const top3Sums = sortedSums.slice(0, 3);
+
+
+const sumOfTop3 = top3Sums.reduce(sumReducer, 0);
+
+
+console.log(sumOfTop3); 
