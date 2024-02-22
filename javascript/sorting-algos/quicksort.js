f9c1680863da6b3a88c8eabf30c1eec82fd8502f
GeSamaras <georgiosamaras@hotmail.com>
2024-02-22 20:36:38 +0100

    quick sorties

---
+++ javascript/sorting-algos/quicksort.js
@@ -0,0 +1,49 @@
+// quicksort high-level description
+// 1. choose a pivot element
+// 2. partition the array to that pivot
+// 3. recursive calls
+    //3a. recursively sort everything to the left of the array
+    //3b. recursively sort  the right
+
+
+// partitioning a pivot bigO(n) linear time, no extra memory
+// picking one element in our array to become the pivot (usually the middle)
+
+
+// pseudocode for partition
+// (called recursively inside the quicksort algo)
+// func Partition (A, left, right) [input = A [l, ...r]]
+    // pivot = A[left],  we initialize the pivot to be the first element
+    // i = left + 1, value of i will be the next index after left
+    // for-loop j = left + 1 all the way to right
+        // if A[j] is less than the pivot
+            // swap A[j] and A[i]
+            // i++
+    // swap A[left] and A[i-1] 
+
+// by definition, partitioning guarantees the pivot will be in the correct place
+
+
+
+// rearrange the array such that 
+// everything less than the pivot is in the left side
+// everything greater is to the right of the pivot
+function quickSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    const pivot = arr[arr.length - 1];
+    const leftArr = [];
+    const rightArr = [];
+  
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] < pivot) {
+        leftArr.push(arr[i]);
+      } else {
+        rightArr.push(arr[i]);
+      }
+    }
+  
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  }
---
+++ javascript/sorting-algos/quicksort.test.js
@@ -0,0 +1,27 @@
+describe('QuickSort', () => {
+    it('should sort an array of numbers', () => {
+      const unsortedArray = [5, 3, 7, 2, 9, 1, 6, 4, 8];
+      const sortedArray = quickSort(unsortedArray);
+      expect(sortedArray).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
+  
+    it('should handle empty array', () => {
+      const emptyArray = [];
+      const sortedArray = quickSort(emptyArray);
+      expect(sortedArray).toEqual([]);
+    });
+  
+    it('should handle array with one element', () => {
+      const singleElementArray = [42];
+      const sortedArray = quickSort(singleElementArray);
+      expect(sortedArray).toEqual([42]);
+    });
+  
+    it('should sort a large array', () => {
+      const unsortedArray = Array.from({ length: 1000 }, () => Math.floor(Math.random() * 1000));
+      const sortedArray = quickSort(unsortedArray);
+      for (let i = 0; i < sortedArray.length - 1; i++) {
+        expect(sortedArray[i] <= sortedArray[i + 1]).toBeTruthy();
+      }
+    });
+  });
